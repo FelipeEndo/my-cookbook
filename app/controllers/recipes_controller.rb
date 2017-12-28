45c109bbc137f0c920cd665a890e4f6ef32dcd27
Felipe Endo <felipecs.endo@gmail.com>
2017-12-28 08:55:42 +0000

    Register Recipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -4,4 +4,22 @@ class RecipesController < ApplicationController
     id = params[:id]
     @recipe = Recipe.find(id)
   end
+  
+  def new
+    @receita = Recipe.new
+  end
+  
+  def create
+    title = params[:recipe][:title]
+    type = params[:recipe][:recipe_type]
+    cuisine = params[:recipe][:cuisine]
+    difficulty = params[:recipe][:difficulty]
+    cook_time = params[:recipe][:cook_time]
+    ingredients = params[:recipe][:ingredients]
+    method = params[:recipe][:method]
+    r = Recipe.new(title: title, recipe_type: type, cuisine: cuisine, difficulty: difficulty, cook_time: cook_time, 
+    ingredients: ingredients, method: method)
+    r.save
+    @recipe = Recipe.find_by title: title
+  end
 end
