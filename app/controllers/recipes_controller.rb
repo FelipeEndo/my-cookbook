5cf4cf05287df2f82570ccbc6b8aa5fc6d2f22e4
Felipe Endo <felipecs.endo@gmail.com>
2017-12-30 05:31:15 +0000

    Remoção do parametro cuisine da table recipe e adição da table Cuisine

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -7,19 +7,28 @@ class RecipesController < ApplicationController
   
   def new
     @receita = Recipe.new
+    options_for_select
+    
   end
   
   def create
     title = params[:recipe][:title]
     type = params[:recipe][:recipe_type]
-    cuisine = params[:recipe][:cuisine]
     difficulty = params[:recipe][:difficulty]
     cook_time = params[:recipe][:cook_time]
     ingredients = params[:recipe][:ingredients]
     method = params[:recipe][:method]
-    r = Recipe.new(title: title, recipe_type: type, cuisine: cuisine, difficulty: difficulty, cook_time: cook_time, 
-    ingredients: ingredients, method: method)
+    cuisine_id = params[:recipe][:cuisine_id]
+    r = Recipe.new(title: title, recipe_type: type, difficulty: difficulty, cook_time: cook_time, 
+    ingredients: ingredients, method: method, cuisine_id: cuisine_id)
     r.save
     @recipe = Recipe.find_by title: title
+    @cuisine = Cuisine.find(cuisine_id).name
   end
 end
+
+  private
+  
+    def options_for_select
+     @cuisine_options_for_select = Cuisine.all.collect {|c| [ c.name, c.id ] }
+    end
