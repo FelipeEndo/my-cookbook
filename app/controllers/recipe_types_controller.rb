80f0794f38525a52b6e004f72dfebb0ed7d4d0b7
Felipe Endo <felipecs.endo@gmail.com>
2017-12-31 09:58:32 +0000

    Admin register recipe types

--- app/controllers/recipe_types_controller.rb
+++ app/controllers/recipe_types_controller.rb
@@ -4,4 +4,18 @@ class RecipeTypesController < ApplicationController
     @recipe_type = RecipeType.find(params[:id])
     @recipes = Recipe.where(cuisine_id: params[:id])
   end
+  
+  def new
+    @recipe_type = RecipeType.new
+  end
+  
+  def create
+    name = params[:recipe_type][:name]
+    @recipe_type = RecipeType.create(name: name)
+    if @recipe_type.save
+      redirect_to @recipe_type
+    else
+      render '_error_messages'
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: 'home#index'
   resources :cuisines, only: [:new, :show, :create]
   resources :recipes, only: [:new, :show, :create]
-  resources :recipe_types, only: [:show]
+  resources :recipe_types, only: [:new, :show, :create]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
